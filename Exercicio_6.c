6f2c1f88039af80afcace94db3468537e2cea62d
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2024-10-02 17:56:52 -0300

    Upload ex 6

--- Exercicio_6.c
+++ Exercicio_6.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main (){
+int main()
+{
+    char nome[50];
+    int qtd;
+    float valor_unit;
+    float valor_total;
 
-    
+    printf("Digite o nome do valor: \n");
+    fgets(nome, 50, stdin);
+
+    printf("Digite a quantidade em estoque: \n");
+    scanf("%d", &qtd);
+
+    printf("Digite o valor do valor unitario do produto: \n");
+    scanf("%f", &valor_unit);
+
+    valor_total = qtd * valor_unit;
+
+    printf("\nValor total: %.2f", valor_total);
 }
