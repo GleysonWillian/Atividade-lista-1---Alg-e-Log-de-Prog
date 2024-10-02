f29f74c2f61a1911f5409fc033d1c678af7dd7b5
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2024-10-02 17:42:20 -0300

    Upload sem erro 2

--- Exercicio_5.c
+++ Exercicio_5.c
@@ -11,7 +11,7 @@ int main()
     printf("Digite o valor do numero B: \n");
     scanf("%d", &B);
 
-    quad = (A^2) + (2*A*B)-(B^2);
+    quad = (A ^ 2) + (2 * A * B) - (B ^ 2);
 
     printf("O valor do quadrado da diferenca e igual a %d.\n", quad);
 
