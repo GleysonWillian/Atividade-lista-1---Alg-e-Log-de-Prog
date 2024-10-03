86dc304c3276a3c4d1e29b4682453495eeb014cb
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2024-10-03 09:44:57 -0300

    subindo exercicio 8

---
+++ Exercicio_8.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    float A, B, C;
+    float delta;
+    float x1, x2;
+
+    printf("Digite o valor de A: \n");
+    scanf("%f", &A);
+
+    getchar();
+
+    printf("Digite o valor de B: \n");
+    scanf("%f", &B);
+    
+    getchar();
+
+    printf("Digite o valor de C: \n");
+    scanf("%f", &C);
+
+    if (A == 0)
+    {
+        printf("Nao e uma equacao do segundo grau. \n");
+    }
+    else
+    {
+        delta = (B * B) - 4 * A * C;
+
+        if (delta > 0)
+        {
+            x1 = (-B + sqrt(delta)) / (2 * A);
+            x2 = (-B - sqrt(delta)) / (2 * A);
+
+            printf("\nOs valores das raizes sao de x1 = %.2f e x2 = %.2f \n", x1, x2);
+        }
+        else if (delta == 0)
+        {
+            x1 = -B / (2 * A);
+            printf("\nOs valores de x1 e x2 sao iguais pois possuem apenas uma raiz real. Entao x1 = x2 = %.2f \n", x1);
+        }
+        else
+            (delta < 0);
+        {
+            printf("\nNao existe raiz real de numero negativo.\n");
+        }
+    }
+    return 0;
+}
